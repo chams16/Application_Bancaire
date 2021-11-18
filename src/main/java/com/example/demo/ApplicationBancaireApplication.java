1605408dea690882a63b65e8b9f4da46e2f1ebd6
chams16 <chamseddine.bahri@eniso.u-sousse.tn>
2021-11-18 11:56:09 +0100

    second commit

---
+++ src/main/java/com/example/demo/ApplicationBancaireApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class ApplicationBancaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationBancaireApplication.class, args);
+	}
+
+
+
+}
