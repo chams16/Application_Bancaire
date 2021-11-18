b8d925c150fb116f57288773e824d62b727bcfe5
chams16 <chamseddine.bahri@eniso.u-sousse.tn>
2021-11-18 15:13:57 +0100

    Building Docker image

--- src/main/java/com/example/demo/ApplicationBancaireApplication.java
+++ src/main/java/com/example/demo/ApplicationBancaireApplication.java
@@ -2,13 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
 public class ApplicationBancaireApplication {
+
+	@GetMapping("/greeting")
+	public String Greeting(){
+		return "hello...";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationBancaireApplication.class, args);
